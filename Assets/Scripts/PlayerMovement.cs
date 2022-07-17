bf2913eb1cd54158d4f2541435aea102f43df03e
Francisco V. Diaz <fevarga1@uc.cl>
2022-07-17 19:54:09 -0400

    donde Entrega02

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Vector3 initialScale = new Vector3(1, 1, 1);
-    public Vector3 direction = Vector3.forward;
+    public int maxHealth = 100;
+    public int currentHealth;
+    public Vector3 initialDirection = Vector3.forward;
+    public Vector3 currentDirection;
     public float speed = 1f;
+
     void Start()
     {
-        transform.localScale = initialScale;
+        currentHealth = maxHealth;
+        int damageTestValue = 50;
+        Damage(damageTestValue);
+        int healTestValue = 10;
+        Heal(healTestValue);
+        SetDirection(initialDirection);
     }
-    private void Update() 
+    private void Update()
     {
-        transform.position += direction * speed * Time.deltaTime;
+        transform.position += currentDirection * speed * Time.deltaTime;
+    }
+    private void SetDirection(Vector3 newDirection)
+    {
+        currentDirection = newDirection;
+        Debug.Log("Current Vector Direction: " + currentDirection);
+    }
+    private void Heal(int hpAmount)
+    {
+        currentHealth += hpAmount;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        DisplayHPStatus();
+    }
+    private void Damage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+        DisplayHPStatus();
+    }
+    private void DisplayHPStatus()
+    {
+        Debug.Log("HP: " + currentHealth + "/" + maxHealth);
     }
 }
